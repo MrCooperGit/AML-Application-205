c74feabab4b456ab2361a514dfe7f9ca7131e7e0
MrCooperGit <100725634+MrCooperGit@users.noreply.github.com>
2023-10-02 10:51:50 +1300

    new file:   .vscode/settings.json 	new file:   aml_application/aml_application/__init__.py 	new file:   aml_application/aml_application/asgi.py 	new file:   aml_application/aml_application/settings.py 	new file:   aml_application/aml_application/urls.py 	new file:   aml_application/aml_application/wsgi.py 	new file:   aml_application/base_app/__init__.py 	new file:   aml_application/base_app/admin.py 	new file:   aml_application/base_app/apps.py 	new file:   aml_application/base_app/forms.py 	new file:   aml_application/base_app/migrations/__init__.py 	new file:   aml_application/base_app/models.py 	new file:   aml_application/base_app/templates/base.html 	new file:   aml_application/base_app/templates/home.html 	new file:   aml_application/base_app/templates/index.html 	new file:   aml_application/base_app/tests.py 	new file:   aml_application/base_app/views.py 	new file:   aml_application/manage.py 	new file:   requirements.txt Initial commit with base_app and urls to templates

---
+++ aml_application/base_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+# from .models import
+# from .forms import SignupForm, LoginForm
+
+
+def home(request):
+    return render(request, 'base_app/home.html')
