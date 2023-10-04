4ef2793bc364a39adb58f3ea45125d2aa6ce368a
Oebus <oliverw4321@gmail.com>
2023-10-05 11:03:49 +1300

    updated settiings for postgreSQL installation

--- aml_application/aml_application/settings.py
+++ aml_application/aml_application/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'aml_application',
+    'base_app',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,16 @@ WSGI_APPLICATION = 'aml_application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aml_application',
+        'USER': 'postgres',
+        'PASSWORD': 'Adminadmin123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        # 'OPTIONS': {
+        #     'service': 'postgresql',
+        #     'passfile': '.my_pgpass',
+        # }
     }
 }
 
