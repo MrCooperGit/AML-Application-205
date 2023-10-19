e81cfdf1d771a55a38c08958a6aa200fbd22e083
Oebus <oliverw4321@gmail.com>
2023-10-19 19:44:03 +1300

    fixing system

--- aml_application/landing_app/views.py
+++ aml_application/landing_app/views.py
@@ -11,20 +11,21 @@ from django.contrib.auth.decorators import login_required
 def index(request, app_name=None):
     
     available_apps = get_available_apps()
+    open_tabs = UserTab.objects.filter(user=request.user)
     
     if app_name is None:
         # means user is at /landing
         # need to redirect to /landing/[first app in database]/ if database is not empty else /landing/default_app
         # this will restart the process and the app name will be set, skipping this if statement
-        if len(available_apps) > 0:
-            app_name = str(available_apps[0])
+        if len(open_tabs) > 0:
+            app_name = open_tabs[0].app_id.name
             return redirect(f'/landing/{app_name}/')
         else:
             app_name = 'default_app'
     else:
         # means user is at /landing/[app name]
         # need to render the app template if the app name is valid else redirect to /landing to restart the process
-        if app_name not in available_apps:
+        if not any(tab.app_id.name == app_name for tab in open_tabs):
             return redirect('/landing')
     
     # user is here if app name is valid
@@ -50,7 +51,7 @@ def add_tab(request, app_name_to_add=None):
     if app_name_to_add not in available_apps:
         return redirect('/landing')
     
-    tab_exists = UserTab.objects.filter(user=request.user).exists()
+    tab_exists = UserTab.objects.filter(user=request.user, app_id=AvailableApps.objects.get(name=app_name_to_add)).exists()
     if tab_exists:
         # future: ask if you want to start form again
         return redirect(f'/landing/{app_name_to_add}/')
