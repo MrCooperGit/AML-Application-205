c443847f862fd59780479dc9d0a621b443f5d14f
Oebus <oliverw4321@gmail.com>
2023-11-14 08:56:14 +1300

    made static file debug mode only

--- aml_application/landing_app/urls.py
+++ aml_application/landing_app/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('<str:app_name>/', views.index, name='app_index'),
     path('tab/add/<str:app_name_to_add>/', views.add_tab, name='add_tab'),
     path('tab/delete/<str:tab_to_delete>/', views.delete_tab, name='delete_tab'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
